7cbb7f03ae77a571d54345ec85d83981c66b1dbb
George Papadopoulos <george.719pap@gmail.com>
2023-05-24 22:36:11 +0300

    Add extended functions

--- src/main/kotlin/Union.kt
+++ src/main/kotlin/Union.kt
@@ -1,5 +1,9 @@
 package kunion
 
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
 /**
  * A discriminated union between [T1] and [T2].
  */
@@ -30,22 +34,46 @@ sealed class Union<T1, T2> {
         }
     }
 
-    fun getT1OrNull(): T1? = when(this) {
+    fun getT1OrNull(): T1? = when (this) {
         is Union.T1 -> this.value
         is Union.T2 -> null
     }
 
-    fun getT2OrNull(): T2? = when(this) {
+    fun getT2OrNull(): T2? = when (this) {
         is Union.T1 -> null
         is Union.T2 -> this.value
     }
+}
 
-    companion object {
-        fun <T1, T2> t1(value: T1): Union<T1, T2> = T1(value)
-        fun <T1, T2> t2(value: T2): Union<T1, T2> = T2(value)
+/**
+ * Returns the encapsulated [Type1][T1] value if this represents [T1] or the result of [action] function for the
+ * encapsulated [Type2][T2] if it is [T2].
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T1: R, T2, R> Union<T1, T2>.getT1OrElse(action: (t2: T2) -> R): R {
+    contract {
+        callsInPlace(action, InvocationKind.AT_MOST_ONCE)
+    }
+    return when (this) {
+        is Union.T1 -> this.value
+        is Union.T2 -> action(this.value)
     }
 }
 
+/**
+ * Returns the encapsulated [Type2][T2] value if this represents [T2] or the result of [action] function for the
+ * encapsulated [Type1][T1] if it is [T1].
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <T1, T2: R, R> Union<T1, T2>.getT2OrElse(action: (t1: T1) -> R): R {
+    contract {
+        callsInPlace(action, InvocationKind.AT_MOST_ONCE)
+    }
+    return when (this) {
+        is Union.T1 -> action(this.value)
+        is Union.T2 -> this.value
+    }
+}
 
 
 sealed class TripleUnion<T1, T2, T3> {
@@ -88,27 +116,21 @@ sealed class TripleUnion<T1, T2, T3> {
         }
     }
 
-    fun getT1OrNull(): T1? = when(this) {
+    fun getT1OrNull(): T1? = when (this) {
         is TripleUnion.T1 -> this.value
         is TripleUnion.T2 -> null
         is TripleUnion.T3 -> null
     }
 
-    fun getT2OrNull(): T2? = when(this) {
+    fun getT2OrNull(): T2? = when (this) {
         is TripleUnion.T1 -> null
         is TripleUnion.T2 -> this.value
         is TripleUnion.T3 -> null
     }
 
-    fun getT3OrNull(): T3? = when(this) {
+    fun getT3OrNull(): T3? = when (this) {
         is TripleUnion.T1 -> null
         is TripleUnion.T2 -> null
         is TripleUnion.T3 -> this.value
-    }
-
-    companion object {
-        fun <T1, T2, T3> t1(value: T1): TripleUnion<T1, T2, T3> = T1(value)
-        fun <T1, T2, T3> t2(value: T2): TripleUnion<T1, T2, T3> = T2(value)
-        fun <T1, T2, T3> t3(value: T3): TripleUnion<T1, T2, T3> = T3(value)
     }
 }
