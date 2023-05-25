5204dec7089ff1a77097f6ce0cb194e2345dfe3f
George Papadopoulos <george.719pap@gmail.com>
2023-05-25 20:47:13 +0300

    Add UnionTemplate.kt

--- src/main/kotlin/UnionTemplate.kt
+++ src/main/kotlin/UnionTemplate.kt
@@ -1,3 +1,12 @@
 package kunion
 
-class UnionTemplate //TODO
+/**
+ * Template for Union types:
+ * Union<T1, T2, ..TN> {
+ *     classes T1, T2, ..TN
+ * }
+ *
+ * - // extensions for every TX
+ * - // transformations for every TX
+ * - // operations "peek" onto value and pipe
+ */
